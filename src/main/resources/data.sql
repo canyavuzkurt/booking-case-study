ff0f1e82949f3377de0b12488f30eb45cbeb81ba
canyavuzkurt <can_yavuzkurt@hotmail.com>
2021-12-27 08:47:43 +0300

    Increased demo data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -22,36 +22,65 @@ INSERT INTO CLEANER (F_NAME, L_NAME, VEHICLE_ID) VALUES ( 'Cleaner2-3', 'Cleaner
 INSERT INTO CLEANER (F_NAME, L_NAME, VEHICLE_ID) VALUES ( 'Cleaner2-4', 'Cleanerson', 2 );
 INSERT INTO CLEANER (F_NAME, L_NAME, VEHICLE_ID) VALUES ( 'Cleaner2-5', 'Cleanerson', 2 );
 
--- INSERT INTO CLEANER (F_NAME, L_NAME, VEHICLE_ID) VALUES ( 'Cleaner3-1', 'Cleanerson', 3 );
--- INSERT INTO CLEANER (F_NAME, L_NAME, VEHICLE_ID) VALUES ( 'Cleaner3-2', 'Cleanerson', 3 );
--- INSERT INTO CLEANER (F_NAME, L_NAME, VEHICLE_ID) VALUES ( 'Cleaner3-3', 'Cleanerson', 3 );
--- INSERT INTO CLEANER (F_NAME, L_NAME, VEHICLE_ID) VALUES ( 'Cleaner3-4', 'Cleanerson', 3 );
--- INSERT INTO CLEANER (F_NAME, L_NAME, VEHICLE_ID) VALUES ( 'Cleaner3-5', 'Cleanerson', 3 );
---
--- INSERT INTO CLEANER (F_NAME, L_NAME, VEHICLE_ID) VALUES ( 'Cleaner4-1', 'Cleanerson', 4 );
--- INSERT INTO CLEANER (F_NAME, L_NAME, VEHICLE_ID) VALUES ( 'Cleaner4-2', 'Cleanerson', 4 );
--- INSERT INTO CLEANER (F_NAME, L_NAME, VEHICLE_ID) VALUES ( 'Cleaner4-3', 'Cleanerson', 4 );
--- INSERT INTO CLEANER (F_NAME, L_NAME, VEHICLE_ID) VALUES ( 'Cleaner4-4', 'Cleanerson', 4 );
--- INSERT INTO CLEANER (F_NAME, L_NAME, VEHICLE_ID) VALUES ( 'Cleaner4-5', 'Cleanerson', 4 );
---
--- INSERT INTO CLEANER (F_NAME, L_NAME, VEHICLE_ID) VALUES ( 'Cleaner5-1', 'Cleanerson', 5 );
--- INSERT INTO CLEANER (F_NAME, L_NAME, VEHICLE_ID) VALUES ( 'Cleaner5-2', 'Cleanerson', 5 );
--- INSERT INTO CLEANER (F_NAME, L_NAME, VEHICLE_ID) VALUES ( 'Cleaner5-3', 'Cleanerson', 5 );
--- INSERT INTO CLEANER (F_NAME, L_NAME, VEHICLE_ID) VALUES ( 'Cleaner5-4', 'Cleanerson', 5 );
--- INSERT INTO CLEANER (F_NAME, L_NAME, VEHICLE_ID) VALUES ( 'Cleaner5-5', 'Cleanerson', 5 );
+INSERT INTO CLEANER (F_NAME, L_NAME, VEHICLE_ID) VALUES ( 'Cleaner3-1', 'Cleanerson', 3 );
+INSERT INTO CLEANER (F_NAME, L_NAME, VEHICLE_ID) VALUES ( 'Cleaner3-2', 'Cleanerson', 3 );
+INSERT INTO CLEANER (F_NAME, L_NAME, VEHICLE_ID) VALUES ( 'Cleaner3-3', 'Cleanerson', 3 );
+INSERT INTO CLEANER (F_NAME, L_NAME, VEHICLE_ID) VALUES ( 'Cleaner3-4', 'Cleanerson', 3 );
+INSERT INTO CLEANER (F_NAME, L_NAME, VEHICLE_ID) VALUES ( 'Cleaner3-5', 'Cleanerson', 3 );
 
-INSERT INTO appointment ( end, start) VALUES ( '2021-12-26 10:00:00', '2021-12-26 08:00:00');
-INSERT INTO appointment ( end, start) VALUES ( '2021-12-25 11:00:00', '2021-12-25 09:00:00');
-INSERT INTO appointment ( end, start) VALUES ( '2021-12-26 10:00:00', '2021-12-26 08:00:00');
-INSERT INTO appointment ( end, start) VALUES ( '2021-12-27 10:00:00', '2021-12-27 08:00:00');
+INSERT INTO CLEANER (F_NAME, L_NAME, VEHICLE_ID) VALUES ( 'Cleaner4-1', 'Cleanerson', 4 );
+INSERT INTO CLEANER (F_NAME, L_NAME, VEHICLE_ID) VALUES ( 'Cleaner4-2', 'Cleanerson', 4 );
+INSERT INTO CLEANER (F_NAME, L_NAME, VEHICLE_ID) VALUES ( 'Cleaner4-3', 'Cleanerson', 4 );
+INSERT INTO CLEANER (F_NAME, L_NAME, VEHICLE_ID) VALUES ( 'Cleaner4-4', 'Cleanerson', 4 );
+INSERT INTO CLEANER (F_NAME, L_NAME, VEHICLE_ID) VALUES ( 'Cleaner4-5', 'Cleanerson', 4 );
+
+INSERT INTO CLEANER (F_NAME, L_NAME, VEHICLE_ID) VALUES ( 'Cleaner5-1', 'Cleanerson', 5 );
+INSERT INTO CLEANER (F_NAME, L_NAME, VEHICLE_ID) VALUES ( 'Cleaner5-2', 'Cleanerson', 5 );
+INSERT INTO CLEANER (F_NAME, L_NAME, VEHICLE_ID) VALUES ( 'Cleaner5-3', 'Cleanerson', 5 );
+INSERT INTO CLEANER (F_NAME, L_NAME, VEHICLE_ID) VALUES ( 'Cleaner5-4', 'Cleanerson', 5 );
+INSERT INTO CLEANER (F_NAME, L_NAME, VEHICLE_ID) VALUES ( 'Cleaner5-5', 'Cleanerson', 5 );
+
+# Saturday
+INSERT INTO appointment ( end, start) VALUES ( '2021-12-25 10:00:00', '2021-12-25 08:00:00');
+INSERT INTO appointment ( end, start) VALUES ( '2021-12-25 11:30:00', '2021-12-25 09:30:00');
+INSERT INTO appointment ( end, start) VALUES ( '2021-12-25 14:00:00', '2021-12-25 10:00:00');
+INSERT INTO appointment ( end, start) VALUES ( '2021-12-25 17:20:00', '2021-12-25 15:20:00');
+INSERT INTO appointment ( end, start) VALUES ( '2021-12-25 20:00:00', '2021-12-25 16:00:00');
+INSERT INTO appointment ( end, start) VALUES ( '2021-12-25 21:15:00', '2021-12-25 17:15:00');
+INSERT INTO appointment ( end, start) VALUES ( '2021-12-25 22:00:00', '2021-12-25 20:00:00');
 
 INSERT INTO cleaner_appointment (appointment_id, cleaner_id) VALUES (1, 1);
-INSERT INTO cleaner_appointment (appointment_id, cleaner_id) VALUES (2, 1);
 INSERT INTO cleaner_appointment (appointment_id, cleaner_id) VALUES (1, 2);
-INSERT INTO cleaner_appointment (appointment_id, cleaner_id) VALUES (2, 2);
 INSERT INTO cleaner_appointment (appointment_id, cleaner_id) VALUES (1, 3);
-INSERT INTO cleaner_appointment (appointment_id, cleaner_id) VALUES (2, 3);
-INSERT INTO cleaner_appointment (appointment_id, cleaner_id) VALUES (3, 4);
-INSERT INTO cleaner_appointment (appointment_id, cleaner_id) VALUES (3, 5);
+INSERT INTO cleaner_appointment (appointment_id, cleaner_id) VALUES (2, 4);
+INSERT INTO cleaner_appointment (appointment_id, cleaner_id) VALUES (2, 5);
+INSERT INTO cleaner_appointment (appointment_id, cleaner_id) VALUES (3, 6);
+INSERT INTO cleaner_appointment (appointment_id, cleaner_id) VALUES (3, 7);
+INSERT INTO cleaner_appointment (appointment_id, cleaner_id) VALUES (4, 8);
 INSERT INTO cleaner_appointment (appointment_id, cleaner_id) VALUES (4, 9);
 INSERT INTO cleaner_appointment (appointment_id, cleaner_id) VALUES (4, 10);
+INSERT INTO cleaner_appointment (appointment_id, cleaner_id) VALUES (5, 11);
+INSERT INTO cleaner_appointment (appointment_id, cleaner_id) VALUES (5, 12);
+INSERT INTO cleaner_appointment (appointment_id, cleaner_id) VALUES (6, 13);
+INSERT INTO cleaner_appointment (appointment_id, cleaner_id) VALUES (7, 14);
+INSERT INTO cleaner_appointment (appointment_id, cleaner_id) VALUES (7, 15);
+
+# Sunday
+INSERT INTO appointment ( end, start) VALUES ( '2021-12-26 10:00:00', '2021-12-26 08:00:00');
+INSERT INTO appointment ( end, start) VALUES ( '2021-12-26 11:30:00', '2021-12-26 09:30:00');
+INSERT INTO appointment ( end, start) VALUES ( '2021-12-26 14:00:00', '2021-12-26 10:00:00');
+INSERT INTO appointment ( end, start) VALUES ( '2021-12-26 17:20:00', '2021-12-26 15:20:00');
+INSERT INTO appointment ( end, start) VALUES ( '2021-12-26 20:00:00', '2021-12-26 16:00:00');
+INSERT INTO appointment ( end, start) VALUES ( '2021-12-26 21:15:00', '2021-12-26 17:15:00');
+INSERT INTO appointment ( end, start) VALUES ( '2021-12-26 22:00:00', '2021-12-26 20:00:00');
+
+INSERT INTO cleaner_appointment (appointment_id, cleaner_id) VALUES (8, 16);
+INSERT INTO cleaner_appointment (appointment_id, cleaner_id) VALUES (8, 17);
+INSERT INTO cleaner_appointment (appointment_id, cleaner_id) VALUES (9, 18);
+INSERT INTO cleaner_appointment (appointment_id, cleaner_id) VALUES (9, 19);
+INSERT INTO cleaner_appointment (appointment_id, cleaner_id) VALUES (10, 20);
+INSERT INTO cleaner_appointment (appointment_id, cleaner_id) VALUES (11, 21);
+INSERT INTO cleaner_appointment (appointment_id, cleaner_id) VALUES (11, 22);
+INSERT INTO cleaner_appointment (appointment_id, cleaner_id) VALUES (12, 23);
+INSERT INTO cleaner_appointment (appointment_id, cleaner_id) VALUES (13, 24);
+INSERT INTO cleaner_appointment (appointment_id, cleaner_id) VALUES (14, 25);
